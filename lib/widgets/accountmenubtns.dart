327d7c52232fb9a1a446831fcaa24b8105fa5f44
Yassmin-Ezzat <125555633+Yassmin-Ezzat@users.noreply.github.com>
2024-12-07 23:30:04 +0200

    Updated accountmenubuttons widget

--- lib/widgets/accountmenubtns.dart
+++ lib/widgets/accountmenubtns.dart
@@ -1,40 +1,46 @@
 import 'package:flutter/material.dart';
 
-Widget buildMenuButton(BuildContext context,
-    {required IconData icon, required String text, required VoidCallback onTap}) {
-  return Material(
-    color: Colors.transparent, // Make Material widget background transparent
-    child: InkWell(
-      onTap: onTap,
-      splashColor: Colors.orange.withOpacity(0.3), // Ripple effect color
-      highlightColor: Colors.orange.withOpacity(0.1), // Highlight color
-      borderRadius: BorderRadius.circular(12), // Optional: Rounded border for the effect
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-        decoration: BoxDecoration(
-          color: Colors.grey[300], // Light gray background
-          borderRadius: BorderRadius.circular(12), // Rounded edges
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
+ Widget buildMenuButton(BuildContext context,
+      {required IconData icon,
+      required String text,
+      required VoidCallback onTap}) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        splashColor: Colors.orange[200], // Ripple effect
+        highlightColor: Colors.orange[100], // Orange shade when pressed
+        borderRadius: BorderRadius.circular(30),
+        child: Ink(
+          decoration: BoxDecoration(
+            color: Colors.white, // Default button background
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(
               children: [
-                Icon(icon, color: Colors.black),
-                SizedBox(width: 10),
+                Icon(icon, color: Colors.black), // Icons remain black
+                SizedBox(width: 20),
                 Text(
                   text,
                   style: TextStyle(
                     fontSize: 16,
                     color: Colors.black,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
               ],
             ),
-            Icon(Icons.arrow_forward_ios, color: Colors.black),
-          ],
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
