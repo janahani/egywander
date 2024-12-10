e8d68f1228a583a411daa81efed3f7177e3cfdf0
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2024-12-10 21:49:51 +0200

    table info submission

--- lib/models/tableinfo.dart
+++ lib/models/tableinfo.dart
@@ -3,7 +3,7 @@ import 'package:uuid/uuid.dart';
 class TableInfo {
   final String id;
   final int tableNumber;
-  final int seatCount;
+  int seatCount;
 
   TableInfo({String? id, required this.tableNumber, required this.seatCount
     }) : id = id ?? const Uuid().v4();
--- lib/screens/OwnerReservationInfoForm.dart
+++ lib/screens/OwnerReservationInfoForm.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:egywander/providers/userProvider.dart';
+import 'loginScreen.dart';
+import 'accountsettingsScreen.dart';
+import 'aboutusScreen.dart';
+import '../widgets/systembars.dart';
+import 'package:egywander/widgets/accountmenubtns.dart';
+import 'OwnerReservationInfoForm.dart';
+import 'viewReservationScreen.dart';
+import '../models/tableinfo.dart';
+import '../widgets/customBtn.dart';
 
 class OwnerReservationInfoForm extends StatefulWidget {
   @override
-  _OwnerReservationInfoFormState createState() => _OwnerReservationInfoFormState();
+  _OwnerReservationInfoFormState createState() =>
+      _OwnerReservationInfoFormState();
 }
 
 class _OwnerReservationInfoFormState extends State<OwnerReservationInfoForm> {
   final _formKey = GlobalKey<FormState>();
-  List<TableInfo> tables = [];
+  List<TableInfo> tables = [
+    TableInfo(tableNumber: 1, seatCount: 4), // Initialize with a default table
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Submit Reservation Info"),
-        backgroundColor: Colors.deepOrange,
-      ),
       backgroundColor: Colors.grey[100],
+      appBar: appBar(context),
+      bottomNavigationBar: bottomNavigationBar(context),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
@@ -26,25 +38,46 @@ class _OwnerReservationInfoFormState extends State<OwnerReservationInfoForm> {
             children: [
               Text(
                 "Add Your Restaurant's Reservation Information",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87),
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87),
               ),
               const SizedBox(height: 20),
-              _buildTableInfoSection(),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: _buildTableInfoSection(),
+                ),
+              ),
+              
+              const SizedBox(height: 10),
+              Center(
+                    child: Container(
+                      width: 180, // Adjust the width as needed
+                      child: CustomButton(
+                        text: "Add Table",
+                        onPressed: _addTable,
+
+                      ),
+                    ),
+                  ),
               const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState?.validate() ?? false) {
-                    // Form submission logic here
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Reservation information submitted successfully')),
-                    );
-                  }
-                },
-                child: Text("Submit"),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.deepOrange,
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  textStyle: TextStyle(fontSize: 16),
+              Center(
+                child: Container(
+                  width: 180, // Adjust the width as needed
+                  child: CustomButton(
+                    text: "Submit Form",
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        // Form submission logic here
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text(
+                                  'Reservation information submitted successfully')),
+                        );
+                      }
+                    },
+                  ),
                 ),
               ),
             ],
@@ -54,61 +87,71 @@ class _OwnerReservationInfoFormState extends State<OwnerReservationInfoForm> {
     );
   }
 
+  void _addTable() {
+    setState(() {
+      tables.add(
+        TableInfo(tableNumber: tables.length + 1, seatCount: 4),
+      );
+    });
+  }
+
   Widget _buildTableInfoSection() {
     return Column(
-      children: [
-        for (int i = 0; i < 3; i++) // You can dynamically add more tables as needed
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Card(
-              elevation: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Table ${i + 1}",
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    Row(
+      children: tables
+          .map((table) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Card(
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
-                          "Seats: ",
-                          style: TextStyle(fontSize: 14, color: Colors.black54),
+                          "Table ${table.tableNumber}",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
                         ),
-                        Container(
-                          width: 40,
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              hintText: '4',
-                              border: OutlineInputBorder(),
-                              contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                        Row(
+                          children: [
+                            Text(
+                              "Seats: ",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.black54),
                             ),
-                            keyboardType: TextInputType.number,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter seat count';
-                              }
-                              return null;
-                            },
-                          ),
+                            Container(
+                              width: 40,
+                              child: TextFormField(
+                                initialValue: table.seatCount.toString(),
+                                decoration: InputDecoration(
+                                  hintText: '4',
+                                  border: OutlineInputBorder(),
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 8),
+                                ),
+                                keyboardType: TextInputType.number,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter seat count';
+                                  }
+                                  return null;
+                                },
+                                onChanged: (value) {
+                                  setState(() {
+                                    table.seatCount =
+                                        int.tryParse(value) ?? table.seatCount;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          ),
-      ],
+              ))
+          .toList(),
     );
   }
-}
-
-class TableInfo {
-  final int tableNumber;
-  final int seatCount;
-
-  TableInfo({required this.tableNumber, required this.seatCount});
 }
