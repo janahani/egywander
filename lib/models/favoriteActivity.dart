ae22a5328cd3b53860229ee981e394da3844d9ea
Yassmin-Ezzat <125555633+Yassmin-Ezzat@users.noreply.github.com>
2024-12-19 00:44:52 +0200

    Added favorites model

--- lib/models/favoriteActivity.dart
+++ lib/models/favoriteActivity.dart
@@ -1,31 +1,28 @@
+// Favorites Model
 class FavoriteActivity {
-  final String placeId; // Unique ID of the place
-  final bool isFavorite; // Indicates if the place is marked as favorite
+  final String userId;
+  final String placeId;
+  final bool isFavorite;
 
-  // Constructor
   FavoriteActivity({
+    required this.userId,
     required this.placeId,
-    this.isFavorite = false, // Default to false if not specified
+    this.isFavorite = true,
   });
 
-  // Convert FavoriteActivity object to Map<String, dynamic> (for saving in Firebase)
   Map<String, dynamic> toMap() {
     return {
+      'userId': userId,
       'placeId': placeId,
       'isFavorite': isFavorite,
     };
   }
 
-  // Create a FavoriteActivity object from Map<String, dynamic> (for fetching from Firebase)
   factory FavoriteActivity.fromMap(Map<String, dynamic> map) {
     return FavoriteActivity(
+      userId: map['userId'] ?? '',
       placeId: map['placeId'] ?? '',
       isFavorite: map['isFavorite'] ?? false,
     );
-  }
-
-  @override
-  String toString() {
-    return 'FavoriteActivity(placeId: $placeId, isFavorite: $isFavorite)';
   }
 }
