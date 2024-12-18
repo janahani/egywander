a980919a18e0538ef9e6d7d53178804d40a80139
janahani <jana2100237@miuegypt.edu.eg>
2024-12-18 11:36:03 +0200

    updates in schedule item

--- lib/widgets/scheduleItem.dart
+++ lib/widgets/scheduleItem.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
-import '/providers/scheduleProvider.dart'; 
+import '/providers/scheduleProvider.dart';
 import '/screens/scheduleScreen.dart';
 
 class ScheduleItem extends StatelessWidget {
   final List<DocumentSnapshot> schedules;
   final String tabName;
+  final bool allowEditDelete;
 
-  ScheduleItem({required this.schedules, required this.tabName});
+  ScheduleItem(
+      {required this.schedules,
+      required this.tabName,
+      required this.allowEditDelete});
 
   @override
   Widget build(BuildContext context) {
@@ -23,65 +27,64 @@ class ScheduleItem extends StatelessWidget {
         DateTime date = DateFormat('dd/MM/yyyy').parse(data['date']);
         String formattedDate = DateFormat('EEEE, d MMM y').format(date);
 
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Date Display
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Text(
-                formattedDate,
-                style: TextStyle(
-                    color: Colors.orange,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold),
-              ),
+        return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          // Date Display
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Text(
+              formattedDate,
+              style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
             ),
-            // Schedule Box
-            Card(
-              margin: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-              child: ListTile(
-                onTap:() {},
-                contentPadding: EdgeInsets.all(12),
-                title: FutureBuilder<String?>(
-                  future: fetchPlaceName(data['placeId']),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Text('Loading...');
-                    } else if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else if (!snapshot.hasData || snapshot.data == null) {
-                      return Text('Place name not found');
-                    } else {
-                      return Text(snapshot.data!);
-                    }
-                  },
-                ),
-                subtitle: Text(
-                  "${data['startingTime']} - ${data['endingTime']}",
-                  style: TextStyle(fontSize: 14, color: Colors.grey[600]),
-                ),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.edit, color: Colors.blue),
-                      onPressed: () {
-                        editSchedule(context, schedules[index]);
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.delete, color: Colors.red),
-                      onPressed: () {
-                        deleteSchedule(schedules[index]);
-                      },
-                    ),
-                  ],
-                ),
+          ),
+          // Schedule Box
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+            child: ListTile(
+              onTap: () {},
+              contentPadding: EdgeInsets.all(12),
+              title: FutureBuilder<String?>(
+                future: fetchPlaceName(data['placeId']),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text('Loading...');
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (!snapshot.hasData || snapshot.data == null) {
+                    return Text('Place name not found');
+                  } else {
+                    return Text(snapshot.data!);
+                  }
+                },
               ),
+              subtitle: Text(
+                "${data['startingTime']} - ${data['endingTime']}",
+                style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+              ),
+              trailing: allowEditDelete
+                  ? Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.edit, color: Colors.blue),
+                          onPressed: () {
+                            editSchedule(context, schedules[index]);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            deleteSchedule(schedules[index]);
+                          },
+                        ),
+                      ],
+                    )
+                  : null, // If editing/deleting is not allowed, hide the buttons
             ),
-          ],
-        );
+          )
+        ]);
       },
     );
   }
