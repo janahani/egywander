71bfc2f689d1f29a304796312f5000253f846e88
janahani <jana2100237@miuegypt.edu.eg>
2024-12-23 16:42:43 +0200

    returned providers

--- lib/providers/userProvider.dart
+++ lib/providers/userProvider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class UserProvider extends ChangeNotifier {
   String? id;
   String? firstName;
@@ -11,10 +10,32 @@ class UserProvider extends ChangeNotifier {
   String? password;
   String? userType;
 
+  // Getter for isLoggedIn - checks if email is set
   bool get isLoggedIn => email != null;
 
-  void login(String id,String firstName, String lastName, String email, int age, String gender, 
-   String password, String userType, userDoc) {
+  String? get userID => id;
+
+   // Setter for userID
+  void setID(String id) {
+    id = id;
+    notifyListeners();  // Notify listeners when user ID is set
+  }
+
+  // Setter for isLoggedIn (to manually set login status)
+  set isLoggedIn(bool status) {
+    if (status) {
+      email = 'yassminezzat@gmail.com';  
+    } else {
+      email = null;
+      password = null;
+      notifyListeners();  
+    }
+    notifyListeners();  
+  }
+
+  // Function to log in the user with all user details
+  void login(String id, String firstName, String lastName, String email, int age, String gender, 
+      String password, String userType, userDoc) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
@@ -26,12 +47,14 @@ class UserProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  // Update user details (e.g., email, password)
   void update(String email, String password, userDoc) {
     this.email = email;
     this.password = password;
     notifyListeners();
   }
 
+  // Function to log out the user (clears all user data)
   void logout() {
     id = null;
     firstName = null;
