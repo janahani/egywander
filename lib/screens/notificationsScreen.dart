0bdbf5b7abd8d414751d62db8b7ca52725fca3da
janahani <jana2100237@miuegypt.edu.eg>
2024-12-23 16:44:08 +0200

    notifications screen done

--- lib/screens/notificationsScreen.dart
+++ lib/screens/notificationsScreen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import '/widgets/notificationscard.dart';
 import '../widgets/systembars.dart';
+import 'package:intl/intl.dart'; // For date formatting
+import 'package:egywander/notificationsDbHelper.dart'; // Import the database helper
 
 class NotificationsScreen extends StatefulWidget {
-  const NotificationsScreen({super.key});
+  final Function onViewedNotifications; // Callback to notify the AppBar
+
+  const NotificationsScreen({required this.onViewedNotifications, Key? key})
+      : super(key: key);
 
   @override
   State<NotificationsScreen> createState() => _NotificationsScreenState();
@@ -11,6 +16,70 @@ class NotificationsScreen extends StatefulWidget {
 
 class _NotificationsScreenState extends State<NotificationsScreen> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
+  final NotificationDbHelper _db = NotificationDbHelper.instance;
+  List<Map<String, dynamic>> _notifications = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadNotifications();
+    _removeExpiredNotifications();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      widget.onViewedNotifications(); // Notify the AppBar
+    });
+  }
+
+  Future<void> _deleteAllNotificationsOnce() async {
+    await _db.clearAllNotifications();
+  }
+
+  Future<void> _loadNotifications() async {
+    List<Map<String, dynamic>> allNotifications =
+        await _db.getNotificationsForToday();
+    DateTime now = DateTime.now();
+
+    setState(() {
+      _notifications = allNotifications.where((notification) {
+        DateTime notificationTime =
+            DateTime.parse(notification['notificationsTime']);
+        DateTime endOfDay = DateTime(
+            now.year, now.month, now.day, 23, 59, 59); // Today's end time
+
+        // Include notifications if notification time has passed and today hasn't ended
+        return now.isAfter(notificationTime) && now.isBefore(endOfDay);
+      }).toList();
+    });
+  }
+
+  Future<void> _removeExpiredNotifications() async {
+    DateTime now = DateTime.now();
+    // Remove notifications from previous days
+    await _db.removeExpiredNotifications();
+  }
+
+  String _formatDuration(Duration duration) {
+    // Format the duration as "X hours Y minutes"
+    int hours = duration.inHours;
+    int minutes = duration.inMinutes % 60;
+    return '$hours hours';
+  }
+
+  String _formatNotificationMessage(Map<String, dynamic> notification) {
+    String placeName = notification['placename'];
+    DateTime tripStartTime = DateTime.parse(notification['startingTime']);
+    DateTime tripEndTime = DateTime.parse(notification['endingTime']);
+
+    // Calculate the duration between start and end time
+    Duration duration = tripEndTime.difference(tripStartTime);
+
+    // Format the duration and generate the message
+    String formattedDuration = _formatDuration(duration);
+
+    // Calculate the exact time the user should be at the place
+    String formattedStartTime = DateFormat('HH:mm').format(tripStartTime);
+
+    return 'Your trip to $placeName today starts at $formattedStartTime. Be there on time and get ready as soon as possible for your $formattedDuration long trip!';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,30 +94,22 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
           top: true,
           child: ListView(
             children: [
-              // Increased padding for ReminderCard
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0), // Add more padding
-                child: ReminderCard(
-                  date: 'Today, 06/12/2024',
-                  location: 'Pyramids of Giza',
-                  time: '3-5 pm',
-                  imageUrl:
-                      'https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHw1fHxweXJhbWlkc3xlbnwwfHx8fDE3MzM1MTMxMTJ8MA&ixlib=rb-4.0.3&q=80&w=400',
-                  isHighlighted: true, // Highlighted card
+              // Display notifications dynamically
+              for (var notification in _notifications)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: ReminderCard(
+                    date: DateFormat('dd/MM/yyyy')
+                        .format(DateTime.parse(notification['date'])),
+                    location: notification['placename'],
+                    // Format the start and end times
+                    time:
+                        '${DateFormat('HH:mm').format(DateTime.parse(notification['startingTime']))} - ${DateFormat('HH:mm').format(DateTime.parse(notification['endingTime']))}',
+                    // Pass the formatted notification message with the duration
+                    formattedNotificationMessage:
+                        _formatNotificationMessage(notification),
+                  ),
                 ),
-              ),
-              SizedBox(height: 10), // Space between cards
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0), // Add more padding
-                child: ReminderCard(
-                  date: 'Yesterday, 05/12/2024',
-                  location: 'Khan El Khalili',
-                  time: '5-7 pm',
-                  imageUrl:
-                      'https://images.unsplash.com/photo-1729956816147-92d4d2ef48ac?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwzfHxraGFuJTIwZWwlMjBraGFsaWxpfGVufDB8fHx8MTczMzUxNDQ1NXww&ixlib=rb-4.0.3&q=80&w=1080',
-                  isHighlighted: false, // Default (not highlighted)
-                ),
-              ),
             ],
           ),
         ),
